d0a87da55a7027e381201fd5d803c3c65ed42ca4
Anthymm <ells.anton04@gmail.com>
2025-05-19 16:24:02 +0200

    working on scroll

--- frontend/src/components/generic/Carousel.vue
+++ frontend/src/components/generic/Carousel.vue
@@ -1,25 +1,42 @@
 <template>
-  <div class="v-carousel-wrapper">
-    <button class="v-carousel-buttons-previous"><Icons icon="arrow-left" /></button>
-    <div class="v-carousel" @scroll="onScroll()" ref="carousel">
-      <slot></slot>
+  <Transition @enter="scrollTo(0)">
+    <div v-if="true" class="v-carousel-wrapper">
+      <button class="v-carousel-buttons-previous">
+        <Icons icon="arrow-left" />
+      </button>
+      <div class="v-carousel" @scroll="onScroll()" ref="carousel">
+        <slot></slot>
+      </div>
+      <button class="v-carousel-buttons-next">
+        <Icons icon="arrow-right" />
+      </button>
     </div>
-    <button class="v-carousel-buttons-next"><Icons icon="arrow-right" /></button>
-  </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
-import { ref, useTemplateRef } from 'vue'
+import { ref, useTemplateRef, onMounted } from 'vue'
 import Icons from './Icons.vue'
 import '../../styles/components/carousel.scss'
 
 const carousel = useTemplateRef('carousel')
+const showCarousel = ref(true)
+var slots
 
 const currentSlot = ref(0)
 
 function checkSlot() {
+}
+
+function scrollTo(child) {
+  carousel.value.children[child].scrollIntoView()
+}
+
+function onScroll() {
   console.log(carousel.value)
 }
 
-function onScroll() {}
+onMounted(() => {
+  slots = carousel.value.children
+})
 </script>
