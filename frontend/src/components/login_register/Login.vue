d56dd51efcfb9aa246ebcc2ff2449319f6afc28c
Anton Ells <ells.anton04@gmail.com>
2025-05-23 06:09:12 +0200

    better styling and disabled button login/register

--- frontend/src/components/login_register/Login.vue
+++ frontend/src/components/login_register/Login.vue
@@ -1,14 +1,16 @@
 <template>
   <section class="user-login-container">
-    <div class="user-login-header">Logga in</div>
+    <div class="user-login-header v-text-large">Logga in</div>
     <div class="user-login-inputs">
-      <label for="userIdentifier">Användarnamn/Email</label>
-      <input type="text" id="userIdentifier" v-model="userIdentifier" />
-      <label for="userPassword">Lösenord</label>
-      <input type="text" id="userPassword" v-model="userPassword" />
+      <label for="userIdentifier" class="v-text-medium">Användarnamn/Email</label>
+      <input type="text" id="userIdentifier" v-model="userIdentifier" class="v-text-input" />
+      <label for="userPassword" class="v-text-medium">Lösenord</label>
+      <input type="text" id="userPassword" v-model="userPassword" class="v-text-input" />
     </div>
     <div class="user-login-action">
-      <button @click="loginUser">Logga in</button>
+      <button :disabled="disableButton" @click="loginUser" class="v-button v-text-medium">
+        Logga in
+      </button>
     </div>
   </section>
 </template>
@@ -17,11 +19,18 @@
 import * as helper from '../../helper'
 import '../../styles/components/loginregister.scss'
 import { useUserStore } from '../../stores/user'
+import { computed } from 'vue'
 
 const user = useUserStore()
 
 const userIdentifier = defineModel('userIdentifier')
 const userPassword = defineModel('userPassword')
+
+const disableButton = computed(() => {
+  if (!userIdentifier.value || !userPassword.value) {
+    return true
+  }
+})
 
 async function loginUser() {
   let paramString = '?userIdentifier=' + userIdentifier.value + '&password=' + userPassword.value
--- frontend/src/components/login_register/Register.vue
+++ frontend/src/components/login_register/Register.vue
@@ -1,16 +1,18 @@
 <template>
   <section class="user-register-container">
-    <div class="user-register-header">Registrera</div>
+    <div class="user-register-header v-text-large">Registrera</div>
     <div class="user-register-inputs">
-      <label for="userEmail">Email:</label>
-      <input type="text" id="userEmail" v-model="userEmail" />
-      <label for="userUsername">Användarnamn:</label>
-      <input type="text" id="userUsername" v-model="userUsername" />
-      <label for="userPassword">Lösenord:</label>
-      <input type="password" id="userPassword" v-model="userPassword" />
+      <label for="userEmail" class="v-text-medium">Email:</label>
+      <input type="text" id="userEmail" v-model="userEmail" class="v-text-input" />
+      <label for="userUsername" class="v-text-medium">Användarnamn:</label>
+      <input type="text" id="userUsername" v-model="userUsername" class="v-text-input" />
+      <label for="userPassword" class="v-text-medium">Lösenord:</label>
+      <input type="password" id="userPassword" v-model="userPassword" class="v-text-input" />
     </div>
     <div class="user-register-action">
-      <button @click="registerUser">Registrera</button>
+      <button :disabled="disableButton" @click="registerUser" class="v-button v-text-medium">
+        Registrera
+      </button>
     </div>
   </section>
 </template>
@@ -19,12 +21,19 @@
 import * as helper from '../../helper'
 import '../../styles/components/loginregister.scss'
 import { useUserStore } from '@/stores/user'
+import { computed } from 'vue'
 
 const user = useUserStore()
 
 const userPassword = defineModel('userPassword')
 const userEmail = defineModel('userEmail')
 const userUsername = defineModel('userUsername')
+
+const disableButton = computed(() => {
+  if (!userUsername.value || !userEmail.value || !userPassword.value) {
+    return true
+  }
+})
 
 function registerUser() {
   let postContent = {
